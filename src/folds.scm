fdf0ca5ab5e4a22789352ff3cc95279518175f4e
ernestrc <ernest@unstable.build>
2025-10-24 06:08:26 +0200

    Add our own folds.scm

---
+++ src/folds.scm
@@ -0,0 +1,21 @@
+[
+  (const_declaration)
+  (expression_case)
+  (default_case)
+  (type_case)
+  (for_statement)
+  (func_literal)
+  (function_declaration)
+  (if_statement)
+  (import_declaration)
+  (method_declaration)
+  (type_declaration)
+  (var_declaration)
+  (composite_literal)
+  (literal_element)
+  (block)
+] @fold
+
+[
+  (import_declaration)
+] @initial_fold
